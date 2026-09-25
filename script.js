53082091d3169c81ea8c5578451ac909c7b4f076
NsovoNkunaSA <nsovo.ike03@gmail.com>
2026-09-25 17:20:52 +0200

    updates

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 
+// =========================
+// MOBILE MENU
+// =========================
+
+const menuButton = document.getElementById("menu-button");
+const navLinks = document.getElementById("nav-links");
+
+
+// Open and close the mobile menu
+
+menuButton.addEventListener("click", function () {
+
+    navLinks.classList.toggle("active");
+
+});
+
+
+// =========================
+// CLOSE MENU AFTER CLICKING
+// =========================
+
+const links = navLinks.querySelectorAll("a");
+
+links.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
